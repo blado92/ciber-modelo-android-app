64caced24ef069f197815f808241d2d0c6e8aa0f
bladodev <bladi.escobar.a@gmail.com>
2025-10-02 21:14:51 -0500

    Version 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.cibermodelo.cibermodeloapplication"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 100001
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
