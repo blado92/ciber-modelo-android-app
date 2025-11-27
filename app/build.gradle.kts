4b3756b490e6c7bc00159982077271d8755d8a54
Bladimir Alvarez <bladi.escobar.a@gmail.com>
2025-11-26 19:43:05 -0500

    Version 1.0.3 (#13)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.cibermodelo.cibermodeloapplication"
         minSdk = 33
         targetSdk = 36
-        versionCode = 100002
-        versionName = "1.0.2"
+        versionCode = 100003
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
